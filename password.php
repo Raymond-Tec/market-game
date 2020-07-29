870cfe94eb29ce651a53ee648f0441aea556824d
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 12:31:11 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- password.php
+++ password.php
@@ -10,8 +10,10 @@ $options = [
 $sha3pw = hash('sha3-512','test');
 $defaultpw = password_hash($pw, PASSWORD_BCRYPT, $options);
 $combinedpw = password_hash($sha3pw, PASSWORD_BCRYPT, $options);
+$combinedpw2 = hash('sha3-512',password_hash($pw, PASSWORD_BCRYPT, $options));
 
-echo $pw.'<br>';
-echo 'SHA3-512 hashed password: ' . $sha3pw .'<br>';
-echo 'Built-in hashed password: ' . $defaultpw .'<br>';
-echo 'Combined password: ' . $combinedpw;
+echo $pw.'<br><br>';
+echo 'SHA3-512 hashed password: ' . $sha3pw .'<br><br>';
+echo 'Built-in hashed password: ' . $defaultpw .'<br><br>';
+echo 'Combined password: ' . $combinedpw . '<br><br>';
+echo 'Combined password 2: ' . $combinedpw2;
