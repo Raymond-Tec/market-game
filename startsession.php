8d693e0bbca037a1ad9e9f67cf059950f41ac040
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 17:24:46 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- startsession.php
+++ startsession.php
@@ -3,6 +3,6 @@ session_start();
 $_SESSION['username'] = $_GET['username'];
 $_SESSION['userid'] = $_GET['id'];
 echo "go fuck yourself";
-header('refresh: 2; Location: index.php');
+header('refresh: 2; url=index.php');
 exit();
 ?>
