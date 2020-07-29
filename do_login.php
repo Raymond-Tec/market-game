0309499d34423f755da4f988b78b253ba2ca7ccd
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 13:56:19 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- do_login.php
+++ do_login.php
@@ -1,12 +1,21 @@
 <?php
-//require 'accessdb.php';
+require 'accessdb.php';
 
-$options = [
+try {
+$stmt = $conn->prepare("SELECT id, username, password, email FROM user WHERE username=".$_POST['username'])
+$stmt->execute();
+$result = $sth->fetch(PDO::FETCH_OBJ);
+}
+catch(PDOException $e) {
+    echo "Error: ".$e->getMessage();
+}
+$conn=null;
+print $result->name;
+
+/*$options = [
     'cost' => 13,
 ];
 
-$hashedpw = password_hash('0o8xQ5!GgK350^N1!lU5jVWB*EHnBJS1', PASSWORD_BCRYPT, $options);
-
-echo $hashedpw;
+$hashedpw = password_hash('0o8xQ5!GgK350^N1!lU5jVWB*EHnBJS1', PASSWORD_BCRYPT, $options);*/
 
 ?>
