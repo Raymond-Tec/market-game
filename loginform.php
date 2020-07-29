5183b950d3859f292f14ac21533d95796cc17162
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 01:06:15 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- loginform.php
+++ loginform.php
@@ -1,1 +1,6 @@
-This is a form.
+<form>
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br>
+</form>
