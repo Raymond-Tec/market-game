a8fa0c10e0bf8232c5574b9a00709c76f785b4fe
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 14:15:00 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- accessdb.php
+++ accessdb.php
@@ -1,7 +1,6 @@
 <?php
 require 'dbcreds.php';
 try {
-    echo "Start Connection<br><br>";
     $conn = new PDO("$dbtype:host=$servername;dbname=$dbname", $username, $password);
     //set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
