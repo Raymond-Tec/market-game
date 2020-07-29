dd36f723b9d04ce828cd2fff38208022a9782447
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-28 21:59:59 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- accessdb.php
+++ accessdb.php
@@ -1,15 +1,10 @@
 <?php
-$severname = "10.132.86.110";
-$username = "marketgame";
-$password = "VDdq!r1uAgF!1R2at8";
-
+require 'dbcreds.php';
 try {
     $conn = new PDO("mysql:host=$severname;dbname=marketgame", $username, $password);
     //set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
-    $conn = null;
-    echo "Connection Closed";
 } catch(PDOException $e) {
     echo "Connection failed:" . $e->getMessage();
 }
