ebb7f5a3f5dac74668990710cbe1d1a64e3df759
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-28 23:41:44 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <?php include 'accessdb.php';?>
+        <?php include htmlspecialchars($_GET["loc"]).'.php';?>
         
         <script src="" async defer></script>
     </body>
