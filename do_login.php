7d14022b1f19238da769b4ae45bcef76cd5208c9
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-29 13:18:42 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- do_login.php
+++ do_login.php
@@ -1,5 +1,15 @@
 <?php
 require 'accessdb.php';
 
+$options = [
+    'cost' => 13,
+];
 
+$hashedpw = password_hash('0o8xQ5!GgK350^N1!lU5jVWB*EHnBJS1', PASSWORD_BCRYPT, $options);
+
+if (password_verify('0o8xQ5!GgK350^N1!lU5jVWB*EHnBJS1', $hashedpw)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid Password.';
+}
 ?>
