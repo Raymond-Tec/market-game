4befca6d292ad3285f125c18e5290741505cfc7d
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 12:07:39 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- news.php
+++ news.php
@@ -7,8 +7,11 @@ if (isset($_SESSION["username"])) {
     $news = $conn->query('SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = \'Published\''); //Query the news table
     while ($row = $news->fetch()) 
     {
+        $author = $conn->prepare('SELECT userid, nickname FROM user WHERE userid = ?'); //Prepare SQL statement to find the Author's nickname from the userid on the post
+        $author->execute([$row['newsauthor']]); //Execute the SQL statement
+        $auth_result = $author->fetch(PDO::FETCH_ASSOC); //Put the statement into an associative array
         echo "<h2>".$row['newstitle']."</h2>";
-        echo "<h4>"."Published On: ".$row['newsdate']." | Written by: ".$row['newsauthor']." | Status: ".$row['newspubpriv'];
+        echo "<h4>"."Published On: ".$row['newsdate']." | Written by: ".$auth_result['nickname']." | Status: ".$row['newspubpriv'];
         echo "<p>".$row['newstext']."</p>";
     }
 } else {
