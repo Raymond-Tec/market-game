936efcad5bf7cd7305650a88c3f26c18ae4fbefa
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 19:57:56 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <div class="col-9">
             <h1><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h1> 
         </div>
-        <div class="col">
+        <div class="col text-center">
             <a href="index.php?loc=news" alt="News">News</a> | 
             <?php
             if (isset($_SESSION['username'])) {
