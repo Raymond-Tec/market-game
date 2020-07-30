95b03751bbf0c4cc2e229de9945d7ceffe33a525
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 10:38:30 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- news.php
+++ news.php
@@ -4,15 +4,14 @@ require 'accessdb.php'; //open DB connection
 
 if (isset($_SESSION["username"])) {
     //Query the news table for all published stories, public and private.
-    $stmt = $pdo->query('SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = \'Published\'');
-    echo "<p>Logged in news.";
-    /*foreach ($stmt as $row)
+    $stmt = $conn->prepare("SELECT newsid, newstitle, newdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = ?");
+    $stmt->execute(['Published']);
+    while ($row = $stmt->fetch()) 
     {
-        echo "<h2>".$row['newstitle']."</h2>";
-        echo "<h4>".$row['newsdate']."</h4>";
-        echo "<p>Author: ".$row['newsauthor']."</p>";
-        echo "<p>".$row['newstext']."</p>";
-    }*/
+        echo $row['newstitle']."\n";
+    }
+
+    echo "<p>Logged in news.";
 } else {
     //Query the news table for all published storeis, public only.
     $stmt = $conn->prepare("SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = 'Published' AND newspubpriv = 'Public'"); //Query the news table
