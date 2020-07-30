6d16be3ec56be3242439dd4292201bb8553b3b52
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 17:24:42 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <h1><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h1> 
         </div>
         <div class="col">
-            <a href="index.php?loc=news" alt="News">News</a>
+            <a href="index.php?loc=news" alt="News">News</a> | 
             <?php
             if (isset($_SESSION['username'])) {
                 echo "<a href=\"index.php?loc=logout\" alt=\"Logout\">Logout</a><br>";
