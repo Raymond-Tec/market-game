2a8d47914d0fec053d9510d726c7aef92ce998a4
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 17:14:10 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- news.php
+++ news.php
@@ -22,16 +22,22 @@ if (!isset($_GET['newsid'])) {
         echo "<p>".$newsreturned."</p>";
     }
 } else {
+    //If news ID is set in the URL, display that specific news item in full.
     $news = $conn->prepare('SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsid = ?');
     $news->execute([$_GET['newsid']]);
     $newsResult = $news->fetch(PDO::FETCH_ASSOC);
-    if ($newsResult['newsstatus']=='Published') {
-        if ($newsResult['newspubpriv']=='Private' && isset($_SESSION['username']) || $newsResult['newspubpriv']=='Public' && !isset($_SESSION['username'])) {
-            $author = ret_nick($newsResult['newsauthor']);
-            echo "<h4><a href=\"index.php?loc=news&newsid=".$newsResult['newsid']."\" title=\"".$newsResult['newstitle']."\">".$newsResult['newstitle']."</a><br><small>";
-            echo "Published On: ".$newsResult['newsdate']." | Written by: ".$author."</small></h4>";
-            echo "<p>".$newsResult['newstext']."</p>";
-        }
+    $author = ret_nick($newsResult['newsauthor']);
+    //If news item is published and public, display it.
+    if ($newsResult['newsstatus']=='Published' && $newsResult['newspubpriv']=='Public') {
+        echo "<h4><a href=\"index.php?loc=news&newsid=".$newsResult['newsid']."\" title=\"".$newsResult['newstitle']."\">".$newsResult['newstitle']."</a><br><small>";
+        echo "Published On: ".$newsResult['newsdate']." | Written by: ".$author."</small></h4>";
+        echo "<p>".$newsResult['newstext']."</p>";
+    //If news item is published and private, make sure the user is logged in.
+    } elseif ($newsResult['newspubpriv']=='Private' && isset($_SESSION['username'])) {
+        echo "<h4><a href=\"index.php?loc=news&newsid=".$newsResult['newsid']."\" title=\"".$newsResult['newstitle']."\">".$newsResult['newstitle']."</a><br><small>";
+        echo "Published On: ".$newsResult['newsdate']." | Written by: ".$author."</small></h4>";
+        echo "<p>".$newsResult['newstext']."</p>";
+    //If the news item isn't published, or is private and user isn't logged in, send back to welcome screen.
     } else {
         header('Location: index.php');
     }
