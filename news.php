1e22c42e2961a86ab2af1fffb16dd9881782445f
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 11:25:28 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- news.php
+++ news.php
@@ -5,11 +5,9 @@ require 'accessdb.php'; //open DB connection
 if (isset($_SESSION["username"])) {
     //Query the news table for all published stories, public and private.
     echo "Here 1.\n";
-    $stmt = $conn->prepare("SELECT newsid, newstitle, newdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = ?");
+    $news = $pdo->query('SELECT newsid, newstitle, newdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = \'Published\'');
     echo "Here 2.\n";
-    $stmt->execute(['Published']);
-    echo "Here 3.\n";
-    while ($row = $stmt->fetch()) 
+    while ($row = $news->fetch()) 
     {
         echo $row['newstitle']."\n";
     }
