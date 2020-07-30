7a0ec6d3de26452d88baf3f2457535bb00579f44
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 11:59:04 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- news.php
+++ news.php
@@ -4,14 +4,13 @@ require 'accessdb.php'; //open DB connection
 
 if (isset($_SESSION["username"])) {
     //Query the news table for all published stories, public and private.
-    echo "Here 1.\n";
     $news = $conn->query('SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = \'Published\''); //Query the news table
-    echo "Here 2.\n";
     while ($row = $news->fetch()) 
     {
-        echo $row['newstitle']."\n";
+        echo "<h2>$row['newstitle']</h2>";
+        echo "<h4>Published On: $row['newsdate'] | Written by: $row['newsauthor'] | Status: $row['newspubpriv']";
+        echo "<p>$row['newstext']</p>";
     }
-    echo "<p>Logged in news.";
 } else {
     //Query the news table for all published storeis, public only.
     $stmt = $conn->prepare("SELECT newsid, newstitle, newsdate, newsauthor, newstext, newsstatus, newspubpriv FROM news WHERE newsstatus = 'Published' AND newspubpriv = 'Public'"); //Query the news table
