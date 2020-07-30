62357549b70cb5b2af3338193a90e24f69dd0cea
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 11:34:11 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- accessdb.php
+++ accessdb.php
@@ -4,6 +4,7 @@ try {
     $conn = new PDO("$dbtype:host=$servername;dbname=$dbname", $username, $password);
     //set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "DB Connection Established.\n\n"
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage() . "<br><br>"; //Error handling
 }
