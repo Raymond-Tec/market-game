56cdda20f0528c46eb3ef22973fd4a89356baf5e
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 20:14:25 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

---
+++ footer.php
@@ -0,0 +1,43 @@
+<div class="container-fluid page-header">
+    <div class="row">
+        <div class="col text-center">
+            <h1><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h1> 
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center">
+            <a href="index.php?loc=news" alt="News">News</a> | 
+            <?php
+            if (isset($_SESSION['username'])) {
+                if ($_SESSION['usergroup']==0) {
+                    echo "<a href=\"index.php?loc=admin\" alt=\"Admin\">Admin</a> | ";
+                }
+                echo "<a href=\"index.php?loc=logout\" alt=\"Logout\">Logout</a>";
+            } else {
+                echo "<a href=\"index.php?loc=loginform\" alt=\"Login\">Login</a>";
+            }
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col text-center">
+            &copy; 2020 Raymond Tec
+        </div>
+    </div>
+    <?php
+    if (isset($_GET['msg']) || isset($_POST['message'])){?>
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6 well-sm text-center">
+        <?php
+            if ($_GET['msg']=="badlogin") {
+                echo "<span class=\"alert-danger\">Incorrect Username or Password.</span>";
+            } elseif ($_GET['msg']=="logoutsuccess") {
+                echo "<span class=\"alert-success\">You have successfully logged out.</span>";
+            }
+        ?>
+        </div>
+        <div class="col-3"></div>
+    </div>
+    <?php } ?>
+</div>
--- index.php
+++ index.php
@@ -22,13 +22,11 @@ if (isset($_SESSION['username']) && (time()-$_SESSION['last_activity']<1800)) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body class="bg-dark">
-        <div class="bg-light container border border-dark">
+        <div class="bg-light container border border-dark rounded-sm">
             <!--[if lt IE 7]>
                 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
             <![endif]-->
-            <div class="container-fluid">
-                <?php include 'header.php';?>
-            </div>
+            <?php include 'header.php';?>
 
             <?php 
                 if ($_GET["loc"]) {
@@ -41,10 +39,12 @@ if (isset($_SESSION['username']) && (time()-$_SESSION['last_activity']<1800)) {
             <div class="container-fluid footer">
 
             </div>
+
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" async></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" async></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" async></script>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" async>
+            <?php include 'footer.php' ?>
         </div>
     </body>
 </html>
