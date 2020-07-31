f7f6ac6a0ce4d964b84a98053873e6149b883677
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 11:58:14 -0400

    Layout adjustment
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- newseditor.php
+++ newseditor.php
@@ -7,14 +7,20 @@
     }
 ?>
 <script src="https://cdn.tiny.cloud/1/w4f9bk41z1tex26ckbeq3lhf53d8txmayk0wvr9ry6yt22ip/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-
 <script>
     tinymce.init({
         selector: '#mytextarea'
     })
 </script>
-<form method="post">
-    <textarea id="mytextarea" name="mytextarea">
-        Hello, World!
-    </textarea>
-</form>
+
+<div class="container bg-light">
+    <div class="row p-3">
+        <div class="col">
+            <form method="post">
+                <textarea id="mytextarea" name="mytextarea">
+                    Hello, World!
+                </textarea>
+            </form>
+        </div>
+    </div>
+</div>
