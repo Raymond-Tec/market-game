4742e1071aca9359e227813b65fb8851030777a9
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 22:24:35 -0400

    Layout adjustments
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- loginform.php
+++ loginform.php
@@ -1,22 +1,25 @@
 <div class="container bg-light">
     <div class="row">
-        <div class="col">
-            <h2>Login</h2>
+        <div class="col-2"></div>
+        <div class="col-10">
+            <h4>Login</h4>
         </div>
     </div>
     <div class="row">
-        <div class="col">
+        <div class="col-1"></div>
+        <div class="col-2">
             <form action="do_login.php" id="loginform" method="post" autocomplete="on">
             <label for="username">Username:</label><br>
             <input type="text" id="username" name="username" required autofocus>
         </div>
-        <div class="col">
+        <div class="col-2">
             <label for="password">Password:</label><br>
             <input type="password" id="password" name="password" required><br>
         </div>
     </div>
     <div class="row">
-        <div class="col">
+        <div class="col-1"></div>
+        <div class="col-2">
             <input type="submit" value="Login">
             </form>
         </div>
