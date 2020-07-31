c6f1561adc2c42d4e9bd315c621a0be7aebf6be7
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 14:33:16 -0400

    Fixing bad logic
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- do_registration.php
+++ do_registration.php
@@ -10,7 +10,7 @@ $newPW2 = $_POST['password2'];
 //Check to make sure passwords match, if they don't send the user back to the registration page.
 if ($newPW1 !== $newPW2) {
     echo "Bad Passwords.";
-    $url="url=index.php?loc=registration&username=".$newUsername."&email=".$newEmail."&nickname=".$newNickname."&msg=badregpw";
+    $url="Location: index.php?loc=registration&username=".$newUsername."&email=".$newEmail."&nickname=".$newNickname."&msg=badregpw";
     header($url);
 }
 
