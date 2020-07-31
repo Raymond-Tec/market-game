5c0556e147d579af363895b35856632464b944fb
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 16:16:50 -0400

    Forgot to set usergroup to 255
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- do_registration.php
+++ do_registration.php
@@ -35,7 +35,7 @@ while ($userResult = $users->fetch()) {
 //You'll want to verify the email address isn't already being used. Will also need to create a forgot password page.
 
 //Finally, do the user record insert.
-$insertUser = $conn->prepare('INSERT INTO users (username, password, usergroup, email, nickname, usercreated, userlastlogin) VALUES (:username,:password,:usergroup,:email,:nickname,:usercreated,:userlastlogin)');
+$insertUser = $conn->prepare('INSERT INTO users (username, password, usergroup, email, nickname, usercreated, userlastlogin) VALUES (:username,:password,255,:email,:nickname,:usercreated,:userlastlogin)');
 $insertUser->execute(['username' => $newUsername, 'password' => $newPW1, 'email'=>$newEmail, 'nickname'=>$newNickname, 'usercreated'=>date('Y-m-d H:i:s'),'userlastlogin'=>date('Y-m-d H:i:s')]);
 $url="Location: index.php?loc=registration&username=".$newUsername."&email=".$newEmail."&nickname=".$newNickname."&msg=goodreg";
 header($url);
