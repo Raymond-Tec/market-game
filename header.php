d7c030a7ee7bbe3ded7f1afcf0e7b5bd5cb35df6
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 20:50:39 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div class="container page-header">
+<div class="container page-header shadow-sm bg-light">
     <div class="row">
         <div class="col-9">
             <h1><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h1> 
