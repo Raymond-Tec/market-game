14d5f365e8e63ebe8c9787e7ab3508cf164345df
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 14:06:02 -0400

    Reorganized form
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- registration.php
+++ registration.php
@@ -9,17 +9,17 @@
             <form action="do_registration.php" class="needs-validation" id="loginform" method="post" autocomplete="off">
             <label for="username">Username:</label><br>
             <input type="text" id="username" class="form-control" name="username" required autofocus>
-            <label for="password1">Enter A Password:</label><br>
-            <input type="password" id="password1" class="form-control" name="password1" required>
-            <label for="password2">Confirm Your Password:</label><br>
-            <input type="password" id="password2" class="form-control" name="password2" required>
-        </div>
-        <div class="col">
             <label for="email">Email:</label><br>
             <input type="email" id="email" class="form-control" name="email" required>
             <label for="text">Nickname:</label><br>
             <input type="text" id="nickname" class="form-control" name="nickname" required>
-            <p class="text-danger well-sm">Make this different from your username to protect your account.</p>
+            <p class="text-danger">Make this different from your username to protect your account.</p>
+        </div>
+        <div class="col">
+            <label for="password1">Enter A Password:</label><br>
+            <input type="password" id="password1" class="form-control" name="password1" required>
+            <label for="password2">Confirm Your Password:</label><br>
+            <input type="password" id="password2" class="form-control" name="password2" required>
             <input type="submit" class="btn btn-primary" value="Login">
             </form>
         </div>
