aa18f8ad2bc0ecb347f60197f00bed70f4dc70d5
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 10:57:24 -0400

    Added user group authentication.
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- admin.php
+++ admin.php
@@ -1,3 +1,9 @@
+<?php
+    $allowed = checkAccess($_SESSION['usergroup'], 0);
+if ($allowed == 0){
+    header('Location: index.php');
+} ?>
+
 <div class="container bg-light">
     <div class="row">
         <div class="col-1"></div>
