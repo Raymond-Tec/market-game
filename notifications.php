60ebbdb538a0fe99aaf6fe1225cb31fcfe168c71
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 10:41:22 -0400

    Layout Adjustment
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- notifications.php
+++ notifications.php
@@ -1,17 +1,19 @@
 <?php if (isset($_GET['msg']) || isset($_POST['message'])){?>
 <div class="container bg-light">
-    <div class="col-3"></div>
-    <div class="col-6 text-center">
-    <?php
-        if ($_GET['msg']=="badlogin") {
-            echo "<span class=\"alert-danger well-sm\">Incorrect Username or Password.</span>";
-        } elseif ($_GET['msg']=="logoutsuccess") {
-            echo "<span class=\"alert-success well-sm\">You have successfully logged out.</span>";
-        } elseif ($_GET['msg']=="loginsuccess") {
-            echo "<span class=\"alert-success well-sm\">You have successfully logged in, ".$_SESSION['nickname'].".";
-        }
-    ?>
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6 text-center">
+        <?php
+            if ($_GET['msg']=="badlogin") {
+                echo "<span class=\"alert-danger well-sm\">Incorrect Username or Password.</span>";
+            } elseif ($_GET['msg']=="logoutsuccess") {
+                echo "<span class=\"alert-success well-sm\">You have successfully logged out.</span>";
+            } elseif ($_GET['msg']=="loginsuccess") {
+                echo "<span class=\"alert-success well-sm\">You have successfully logged in, ".$_SESSION['nickname'].".";
+            }
+        ?>
+        </div>
+        <div class="col-3"></div>
     </div>
-    <div class="col-3"></div>
 </div>
 <?php } ?>
