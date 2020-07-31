f780c0a4c791c70142af4bbba65d4c40e30e1c1a
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-30 23:00:48 -0400

    Added admin page and news editor
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

---
+++ admin.php
@@ -0,0 +1,8 @@
+<div class="container bg-light">
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col">
+            <a href="index.php?loc=newseditor" alt="News Editor">News Editor</a>
+        </div>
+    </div>
+</div>
---
+++ newseditor.php
@@ -0,0 +1,12 @@
+<script src="https://cdn.tiny.cloud/1/w4f9bk41z1tex26ckbeq3lhf53d8txmayk0wvr9ry6yt22ip/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+<script>
+    tinymce.init({
+        selector: '#mytextarea'
+    })
+</script>
+<form method="post">
+    <textarea id="mytextarea" name="mytextarea">
+        Hello, World!
+    </textarea>
+</form>
