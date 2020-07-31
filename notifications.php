d630f9a62f1ddeb119982e5d00cf59d636d1ddfe
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 10:39:49 -0400

    Layout adjustment
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- notifications.php
+++ notifications.php
@@ -1,14 +1,14 @@
 <?php if (isset($_GET['msg']) || isset($_POST['message'])){?>
 <div class="container bg-light">
     <div class="col-3"></div>
-    <div class="col-6 well-sm text-center">
+    <div class="col-6 text-center">
     <?php
         if ($_GET['msg']=="badlogin") {
-            echo "<span class=\"alert-danger\">Incorrect Username or Password.</span>";
+            echo "<span class=\"alert-danger well-sm\">Incorrect Username or Password.</span>";
         } elseif ($_GET['msg']=="logoutsuccess") {
-            echo "<span class=\"alert-success\">You have successfully logged out.</span>";
+            echo "<span class=\"alert-success well-sm\">You have successfully logged out.</span>";
         } elseif ($_GET['msg']=="loginsuccess") {
-            echo "<span class=\"alert-success\">You have successfully logged in, ".$_SESSION['nickname'].".";
+            echo "<span class=\"alert-success well-sm\">You have successfully logged in, ".$_SESSION['nickname'].".";
         }
     ?>
     </div>
