8a0aa21fd5066aedb4ef9594eecfe21bdbbf7122
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 11:54:40 -0400

    Layout Adjustment
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-        <div class="col text-center">
-            <h3><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h3> 
-        </div>
+<div class="row">
+    <div class="col text-center">
+        <h3><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h3> 
     </div>
-    <div class="row p-2">
-        <div class="col text-right">
-            &copy; 2020 Raymond Tec
-        </div>
+</div>
+<div class="row p-2">
+    <div class="col text-right">
+        &copy; 2020 Raymond Tec
+    </div>
+</div>
--- header.php
+++ header.php
@@ -1,7 +1,9 @@
+<div class="row">
     <div class="col-9">
         <h1><a href="index.php" alt="<?php echo $gameName?>"><?php echo $gameName; ?></a></h1> 
     </div>
     <div class="col text-right">
         <?php include 'navigation.php'; ?>
     </div>
+</div>
 <?php include 'notifications.php' ?>
