f35d4f22bb1d7d7c977e055a5b2f7c3dc0182d11
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-07-31 16:21:26 -0400

    Forgot what the usergroup value should have been
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- do_registration.php
+++ do_registration.php
@@ -36,7 +36,7 @@ while ($userResult = $users->fetch()) {
 
 //Finally, do the user record insert.
 try {
-    $insertUser = $conn->prepare('INSERT INTO user (username, password, usergroup, email, nickname, usercreated, userlastlogin) VALUES (:username,:password,255,:email,:nickname,:usercreated,:userlastlogin)');
+    $insertUser = $conn->prepare('INSERT INTO user (username, password, usergroup, email, nickname, usercreated, userlastlogin) VALUES (:username,:password,127,:email,:nickname,:usercreated,:userlastlogin)');
     $insertUser->execute(['username' => $newUsername, 'password' => $newPW1, 'email'=>$newEmail, 'nickname'=>$newNickname, 'usercreated'=>date('Y-m-d H:i:s'),'userlastlogin'=>date('Y-m-d H:i:s')]);
     $url="Location: index.php?loc=registration&username=".$newUsername."&email=".$newEmail."&nickname=".$newNickname."&msg=goodreg";
     header($url);
