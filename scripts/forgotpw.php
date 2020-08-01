e08799fd4098eb43974d0b66f896a511d22bed94
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-01 18:20:29 -0400

    Updated formatting
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/forgotpw.php
+++ scripts/forgotpw.php
@@ -5,12 +5,14 @@
         </div>
     </div>
     <div class="row">
+        <div class="col-1"></div>
         <div class="col">
             <form action="scripts/do_fpw.php" class="needs-validation" id="fpwform" method="post" autocomplete="on">
-            <label for="username">Email Address:</label><br>
-            <input type="text" id="email" class="form-control" name="username" required autofocus>
-            <input type="submit" class="btn btn-primary" value="Submit">
+                <label for="username">Email Address:</label><br>
+                <input type="text" id="email" class="form-control" name="username" required autofocus><br>
+                <input type="submit" class="btn btn-primary" value="Submit">
             </form>
         </div>
+        <div class="col-1"></div>
     </div>
 </div>
