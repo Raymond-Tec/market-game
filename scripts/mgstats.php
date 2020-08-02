2c0f7fba9fa154b1263753b5648bb3123a9c0500
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-02 01:39:53 -0400

    Updated to include Simple Maps link
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/mgstats.php
+++ scripts/mgstats.php
@@ -1,5 +1,6 @@
 <?php
     $gameName = "The Market Game";
     $gameDomain = "themarketgame.com";
+    $geoDataLink = "City Geographic Data provided by <a href=\"https://simplemaps.com/data/us-cities\" alt=\"Simple Maps\" target=\"_blank\">Simple Maps</a>.";
     $ver = "0.0.1";    
 ?>
