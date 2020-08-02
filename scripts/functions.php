32354f22d681911ed48d477cc15db1c4ea240a55
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-01 21:33:55 -0400

    Re-added From headers to sendmail.
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/functions.php
+++ scripts/functions.php
@@ -51,6 +51,7 @@ function checkAccess($group, $accessRequired) {
 function sendMail($uemail, $subj, $body) {
     require 'mgstats.php';
     $msg=wordwrap($body,70);
-    mail($uemail,$subj,$msg);
+    $headers = 'From: no-reply@'.$gameName.'.com'."\r\n".'Reply-To: admin@'.$gameName.'.com'."\r\n".'X-Mailer: PHP/'.phpversion();
+    mail($uemail,$subj,$msg,$headers);
 }
 ?>
