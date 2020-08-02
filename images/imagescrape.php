d1a2952cc314f352f6c99686d150db8cd1ba309b
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-02 11:14:45 -0400

    Messages
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- images/imagescrape.php
+++ images/imagescrape.php
@@ -1,5 +1,8 @@
 <?php
     $input = 'https://thispersondoesnotexist.com/image';
+    echo "URL to open: ".$input."<br><br>";
     $output = 'image.jpg';
+    echo "File to be saved to: ".$output."<br><br>";
     file_put_contents($output, file_get_contents($input));
+    echo "See the <a href=\"".$output."\">file here</a>";
 ?>
