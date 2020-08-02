73ecccc426511ee90bbe1a59d4f09289339bea6f
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-02 11:31:58 -0400

    Try again...
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- images/imagescrape.php
+++ images/imagescrape.php
@@ -1,9 +1,11 @@
 <?php
-    $input = 'https://thispersondoesnotexist.com/image';
-    echo "URL to open: ".$input."<br><br>";
-    $output = './image.jpg';
-    echo "File to be saved to: ".$output."<br><br>";
-    echo file_get_contents($input);
-    //file_put_contents($output, file_get_contents($input));
-    echo "See the <a href=\"".$output."\">file here</a>";
+    $handle = $curl_init();
+    $url = "https://thispersondoesnotexist.com/image";
+
+    curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+    $output = curl_exec($handle);
+    curl_close($handle);
+    echo $output;
 ?>
