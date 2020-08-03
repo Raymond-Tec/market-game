1b980f7fcd3320fe5fd7bec2a6e51c40ba5aa788
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 19:25:51 -0400

    Updated
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/generatebusinesses.php
+++ scripts/generatebusinesses.php
@@ -8,62 +8,31 @@ $conn=accessdb();
 echo "Successful.<br>";
 
 //Grab NAICS codes from database
-$naics = $conn->query('SELECT naics_id FROM naics');
-$naics->execute();
-$naicsResult = $naics->fetch(PDO::FETCH_NUM);
-echo "Number of NAICS Codes in database: ".count($naicsResult)."<br>";
+$naics = $conn->query('SELECT naics_id FROM naics')->fetchAll(PDO::FETCH_NUM);
+echo "Number of NAICS Codes in database: ".count($naics)."<br>";
 
 //Grab geographic locations with more than 20,000 in population
-$geo = $conn->query('SELECT id, `population` FROM geodata WHERE `population` >= 20000');
-$geo->execute();
-$geoResult = $naics->fetch(PDO::FETCH_NUM);
-echo "Locations with more than 20,000: ".count($geoResults)."<br>";
+$geo = $conn->query('SELECT id, `population` FROM geodata WHERE `population` >= 20000')->fetchAll(PDO::FETCH_NUM);
+echo "Locations with more than 20,000: ".count($geo)."<br>";
 
 /*
-//Import female first names into an array (4639)
-$femaleFN = explode(PHP_EOL,file_get_contents('popular-girls-names.csv'));
+//Import business names into an array and count total number of business names.
+$busName = explode(PHP_EOL,file_get_contents('business-names.csv'));
+$totalBus = count($busName);
 
-//Import male first names into an array (4629)
-$maleFN = explode(PHP_EOL,file_get_contents('popular-boys-names.csv'));
-
-//Import surnames into an array (2001)
-$surnames = explode(PHP_EOL,file_get_contents('surnames.csv'));
-
-echo "Available Female First Names: ".count($femaleFN)."<br>";
-echo "Available Male First Names: ".count($maleFN)."<br>";
-echo "Available Surnames: ".count($surnames)."<br>";
+echo "Total Business Names: ".$totalBus."<br>";
 
 //Display table of what's going to be output by this script
-for ($x=0; $x < 1058; $x++) {
-    $photo = $x + 1;
-    $botfirstname = $femaleFN[rand(0,4638)];
-    $botlastname = $surnames[rand(0,2000)];
-    $botphoto = 'femprof_(' . $photo . ').jpg';
-    
-    echo "Inserting: ".$x." ".$botfirstname." ".$botlastname." ".$botphoto."...";
+for ($x=0; $x < $totalBus; $x++) {
+    echo "Inserting: ".$x." of ".$totalBus.". Name: ".$busName[$x]."...";
     try {
-        $insertBot = $conn->prepare('INSERT INTO botplayer (botfirstname, botlastname, botphoto) VALUES (:bfn, :bln, :bp)');
-        $insertBot->execute(['bfn' => $botfirstname, 'bln' => $botlastname, 'bp' => $botphoto]);
+        $insertBus = $conn->prepare('INSERT INTO businesses (businessname, industryid, location_id) VALUES (:bn, :iid, :lid)');
+        $insertBot->execute(['bn' => $busName[$x], 'iid' => $naicsResult[rand(0,count($naicsResult))], 'lid' => $geoResult[rand(0,count($geoResult))]]);
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
     echo "Successful.<br>";
 }
 
-for ($x=0; $x < 984; $x++) {
-    $photo = $x + 1;
-    $botfirstname = $maleFN[rand(0,4638)];
-    $botlastname = $surnames[rand(0,2000)];
-    $botmoney = 10000.00;
-    $botphoto = 'maleprof_(' . $photo . ').jpg';
-
-    echo "Inserting: ".$x." ".$botfirstname." ".$botlastname." ".$botphoto."...";
-    try {
-        $insertBot = $conn->prepare('INSERT INTO botplayer (botfirstname, botlastname, botphoto) VALUES (:bfn, :bln, :bp)');
-        $insertBot->execute(['bfn' => $botfirstname, 'bln' => $botlastname, 'bp' => $botphoto]);
-    } catch(PDOException $e) {
-        echo $e->getMessage();
-    }
-    echo "Successful.<br>";
-}*/
+*/
 ?>
