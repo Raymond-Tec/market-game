cab3be262b6351856c88bd9367530eb792df86a8
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 19:35:48 -0400

    Updated to read business names file
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/generatebusinesses.php
+++ scripts/generatebusinesses.php
@@ -15,13 +15,13 @@ echo "Number of NAICS Codes in database: ".count($naics)."<br>";
 $geo = $conn->query('SELECT id, `population` FROM geodata WHERE `population` >= 5000')->fetchAll(PDO::FETCH_NUM);
 echo "Locations with more than 5,000: ".count($geo)."<br>";
 
-/*
+
 //Import business names into an array and count total number of business names.
 $busName = explode(PHP_EOL,file_get_contents('business-names.csv'));
 $totalBus = count($busName);
 
 echo "Total Business Names: ".$totalBus."<br>";
-
+/*
 //Display table of what's going to be output by this script
 for ($x=0; $x < $totalBus; $x++) {
     echo "Inserting: ".$x." of ".$totalBus.". Name: ".$busName[$x]."...";
