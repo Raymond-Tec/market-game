98f0ead8222f36ce55c09c612bec220d3336ac34
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 14:27:10 -0400

    Updated logevents.
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/functions.php
+++ scripts/functions.php
@@ -19,7 +19,7 @@ function logevent($action) {
     if($_SESSION['username']) { $remoteUN = $_SESSION['username']; } else { $remoteUN = "Not Logged In"; } 
     $logTime = date('Y-m-d H:i:s'); //Establish Log time.
     $entry = $logTime." ".$action." ".$_SERVER['REMOTE_ADDR']." ".$_SERVER['HTTP_USER_AGENT']." ".$remoteUN; //Create the variable for log entry
-    $filename = date('Y-m-d').".log"; //Build filename, changes by day.
+    $filename = "logs/".date('Y-m-d').".log"; //Build filename, changes by day.
     echo "Writing ".$entry." to logs/".date('Y-m-d').'.log'; //Debug statement.
     file_put_contents($filename,$entry, FILE_APPEND); //Appends the entry to the log file.
     return;
