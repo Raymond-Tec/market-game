dbe0c3eeaac44db64d4bd2d778907f1d0bcc99ed
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-02 23:01:57 -0400

    Trying to get API
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/businessnames.php
+++ scripts/businessnames.php
@@ -7,7 +7,10 @@ $apiKey = 'X-Api-Key: 478ca5d614ed432b828833a79d2b012d'; // MY API key
 
 $curl = curl_init($url); //Create the session
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HEADER, $apiKey);
+curl_setopt($curl, CURLOPT_HEADER, [
+    'startingWords: null',
+    'X-Api-Key: 478ca5d614ed432b828833a79d2b012d'
+]);
 
 $response = curl_exec($curl);
 curl_close($curl);
