114b4d0590706291f5082cffefb554c9b6402a51
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 17:50:03 -0400

    Updated
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/generateainames.php
+++ scripts/generateainames.php
@@ -28,7 +28,7 @@ for ($x=0; $x <= 1057; $x++) {
     try {
         echo "Attempting to insert: ".$x." ".$botfirstname.$botlastname." ".$botphoto."<br>";
         $insertBot = $conn->prepare('INSERT INTO botplayer (botid, botfirstname, botlastname, botphoto) VALUES (:bid, :bfn, :bln, :bp');
-        $insertBot->execute(['bid' => $x, 'bfn' => $botfirstname, 'bln' => $botlastname, 'bp' => $botphoto]);
+        $insertBot->execute(['bid'=>$x,'bfn'=>$botfirstname,'bln'=>$botlastname,'bp'=>$botphoto]);
         echo "Successfully inserted: ".$x." ".$botfirstname.$botlastname." ".$botphoto."<br>";
     } catch(PDOException $e) {
         echo $insertBot . "<br>" . $e->getMessage();
@@ -45,7 +45,7 @@ for ($x=0; $x <= 983; $x++) {
     try {
         echo "Attempting to insert: ".$x." ".$botfirstname.$botlastname." ".$botphoto."<br>";
         $insertBot = $conn->prepare('INSERT INTO botplayer (botid, botfirstname, botlastname, botphoto) VALUES (:bid, :bfn, :bln, :bp');
-        $insertBot->execute(['bid' => $x, 'bfn' => $botfirstname, 'bln' => $botlastname, 'bp' => $botphoto]);
+        $insertBot->execute(['bid'=>$x,'bfn'=>$botfirstname,'bln'=>$botlastname,'bp'=>$botphoto]);
         echo "Successfully inserted: ".$x." ".$botfirstname.$botlastname." ".$botphoto."<br>";
     } catch(PDOException $e) {
         echo $insertBot . "<br>" . $e->getMessage();
