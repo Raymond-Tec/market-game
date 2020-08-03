0e50d292816276335c8c9ef3063f3183ee137a21
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 13:32:41 -0400

    Updated
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/generateainames.php
+++ scripts/generateainames.php
@@ -2,7 +2,7 @@
 require 'functions.php';
 require 'mgstats.php';
 
-$femaleFN = explode("|",file_get_contents('popular-girls-names.csv'));
+$femaleFN = explode(" ",file_get_contents('popular-girls-names.csv'));
 echo $femaleFN[0];
 
 ?>
