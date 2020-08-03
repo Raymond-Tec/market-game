34542d5c0eb7ac26de2715d5679447748ac8f6dc
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 14:07:26 -0400

    Fixing logevent
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/functions.php
+++ scripts/functions.php
@@ -19,8 +19,8 @@ function logevent($action) {
     if($_SESSION['username']) { $remoteUN = $_SESSION['username']; } else { $remoteUN = "Not Logged In"; } 
     $logTime = date('Y-m-d H:i:s');
     $entry = $logTime." ".$action." ".$_SERVER['REMOTE_ADDR']." ".$_SERVER['HTTP_USER_AGENT']." ".$remoteUN;
-    echo "Writing ".$entry." to ".$gamePath.'//logs//'.date('Y-m-d').'.log';
-    file_put_contents($gamePath.'/logs/'.date('Y-m-d').'.log',$entry, FILE_APPEND);
+    echo "Writing ".$entry." to logs/".date('Y-m-d').'.log';
+    file_put_contents("logs/".date('Y-m-d').".log",$entry, FILE_APPEND);
     return;
 }
 
