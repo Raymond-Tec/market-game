6732478136df026b0d0f9b67343a800091850c97
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-02 23:21:37 -0400

    trying again.
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/businessnames.php
+++ scripts/businessnames.php
@@ -2,15 +2,13 @@
 //https://randommer.io/api/Name/Suggestions
 //X-Api-Key: 478ca5d614ed432b828833a79d2b012d
 
-$url = 'https://randommer.io/api/Name/Suggestions?startingWords=null'; // URL to site
+$url = 'https://randommer.io/api/Name?nameType=fullname&quantity=1000'; // URL to site
 $apiKey = 'X-Api-Key: 478ca5d614ed432b828833a79d2b012d'; // MY API key
 
 $curl = curl_init($url); //Create the session
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HEADER, [
-    'accept: */*',
-    'X-Api-Key: 478ca5d614ed432b828833a79d2b012d',
-    'Content-Type: application/json'
+    $apiKey
 ]);
 
 $response = curl_exec($curl);
