386514e89aa13657d890c95dd74c16bcd6e980c8
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 22:56:23 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/fnddupes.php
+++ scripts/fnddupes.php
@@ -1,12 +1,14 @@
 <?php
 require 'functions.php';
 
+echo "Accessing Database...";
 $conn=accessdb();
+echo "Success.<br><br>";
 
 try {
-    $dupes = $conn->prepare('SELECT businessname, COUNT(businessname) FROM businesses GROUP BY businessname HAVING COUNT(businessname)>1')->fetchAll(PDO::FETCH_COLUMN);
+    $dupes = $conn->query('SELECT businessname, COUNT(businessname) FROM businesses GROUP BY businessname HAVING COUNT(businessname)>1')->fetchAll(PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
-echo $dupes[0];
+echo $dupes[0][businessname];
 ?>
