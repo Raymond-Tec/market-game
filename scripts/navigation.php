7a7f953fe8b894a8f9119a0f46c102d5bed95376
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-04 12:12:22 -0400

    Updating layout
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/navigation.php
+++ scripts/navigation.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-xl navbar-light bg-light text-right">
+<nav class="navbar navbar-expand-xl navbar-light bg-light justify-content-end">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
