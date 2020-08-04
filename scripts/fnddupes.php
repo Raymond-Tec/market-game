c28e51109a2a72b115a03e4ea5028f6872677b69
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 23:28:28 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/fnddupes.php
+++ scripts/fnddupes.php
@@ -11,20 +11,24 @@ try {
     echo $e->getMessage();
 }
 $totDupes = count($dupes);
-echo $totDupes;
-/*
-for ($x=0; $x > $totDupes-1; $x++) {
+echo "Total Duplicates Found: ".$totDupes."<br><br>";
+
+for ($x=0; $x < $totDupes-1; $x++) {
     try {
-        $findDupe = $conn->query('SELECT * FROM businesses WHERE businessname = \''.$dupes[0]['businessname'].'\'')->fetchAll(PDO::FETCH_ASSOC);
+        $findDupe = $conn->query('SELECT * FROM businesses WHERE businessname = \''.$dupes[$x]['businessname'].'\'')->fetchAll(PDO::FETCH_ASSOC);
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
 
-    for ($y=1; $x > )
-    try {
-        $delDupe = $conn->query('DELETE FROM businesses WHERE businessid = \''.$findDupe[1]['businessid'].'\'')->execute();
-    } catch(PDOException $e) {
-        echo $e->getMessage();
+    echo "Deleting ".$dupes[$x]['COUNT(businessname)']." of ".$totDupes.". Record: ".$dupes[$x]['businessname']."<br>";
+    $curDupes = count($findDupe);
+
+    for ($y=1; $y > $curDupes-1; $y++) {
+        try {
+            $delDupe = $conn->query('DELETE FROM businesses WHERE businessid = \''.$findDupe[$y]['businessid'].'\'')->execute();
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-}*/
+}
 ?>
