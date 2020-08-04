3765d5afc71248498cf2b1c818fa9cb6d6ae0994
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 21:55:47 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/generatebusinesses.php
+++ scripts/generatebusinesses.php
@@ -9,11 +9,11 @@ echo "Successful.<br><br>";
 
 //Grab NAICS codes from database
 $naics = $conn->query('SELECT naics_id FROM naics')->fetchALL(PDO::FETCH_COLUMN);
-echo "Number of NAICS Codes in database: ".count($naics)." Random Industry: ".$naics[rand(0,count($naics))]."<br><br>";
+echo "Number of NAICS Codes in database: ".count($naics)." Random Industry: ".$naics[rand(0,count($naics)-1)]."<br><br>";
 
 //Grab geographic locations with more than 5,000 in population
 $geo = $conn->query('SELECT id, `population` FROM geodata WHERE `population` >= 5000')->fetchAll(PDO::FETCH_ASSOC);
-echo "Locations with more than 5,000: ".count($geo)." Random Location: ".$geo[rand(0,count($geo))][id]."<br>";
+echo "Locations with more than 5,000: ".count($geo)." Random Location: ".$geo[rand(0,count($geo)-1)][id]."<br>";
 
 //Import business names into an array and count total number of business names.
 $busName = explode(PHP_EOL,file_get_contents('business-names.csv'));
@@ -28,8 +28,8 @@ for ($x=0; $x < $totalBus; $x++) {
 
 //Insert into the businesses table
 for ($x=0; $x < $totalBus; $x++) {
-    $naicsRand = rand(0,count($naics));
-    $geoRand = rand(0,count($geo));
+    $naicsRand = rand(0,count($naics)-1);
+    $geoRand = rand(0,count($geo)-1);
     echo "Inserting: ".$x." of ".$totalBus.". Name: ".$busName[$x]."Industry ID: ".$naics[$naicsRand]." Geographic ID: ".$geo[$geoRand][id]."...";
     try {
         $insertBus = $conn->prepare('INSERT INTO businesses (businessname, industryid, location_id) VALUES (:bn, :iid, :lid)');
