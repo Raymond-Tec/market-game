d4e096609c4489998e24432e725f11cd2fb4b298
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-03 22:57:35 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/fnddupes.php
+++ scripts/fnddupes.php
@@ -10,5 +10,6 @@ try {
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
-echo $dupes[0][businessname]." ".$dupes[0][count];
+var_dump($dupes);
+//echo $dupes[0][businessname]." ".$dupes[0][count];
 ?>
