3faa737b714cc43c6cd2a542857735e5b8e11fdb
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-04 12:11:47 -0400

    Layout Update
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/navigation.php
+++ scripts/navigation.php
@@ -3,7 +3,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse text-right" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Site</a>
