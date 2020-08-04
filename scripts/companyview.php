a16dc4a3fe26884be980bbda7e182fbeda911210
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-04 14:45:44 -0400

    Pagination
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/companyview.php
+++ scripts/companyview.php
@@ -4,36 +4,71 @@ $conn=accessdb();
 
 //If business ID is not set in the URL, display all businesses
 if (!isset($_GET['busid']) && isset($_SESSION['username'])) {
-    //Query the news table for all published storeis, public only.
+    logevent('Viewed all businesses'); //Log the event
+    
     $totRecs = $conn->query('SELECT COUNT(*) FROM businesses')->fetchColumn();
     $totPages = round($totRecs/25,0);
-    echo "Total Records: ".$totRecs."| Total Pages: ".$totPages."<br>";
-    
-    $bus = $conn->query('SELECT businessid, businessname, industryid, location_id FROM businesses');
-    logevent('Viewed all businesses');
-    $x = 0;
-    
-    while ($busResult = $bus->fetch()) 
-    {
-        try {
-            $naics = $conn->prepare('SELECT naics_id, naics_description FROM naics WHERE naics_id = ?');
-            $naics->execute([$busResult['industryid']]);
-            $naicsResult = $naics->fetch(PDO::FETCH_ASSOC);
-        } catch(PDOException $e) {
-            echo $e->getMessage();
+    echo "Page ID: ".$_GET['pageid']."<br>";
+    ?>
+
+
+
+    <?php
+    if (!isset($_GET['pageid'])) {
+        $bus = $conn->query('SELECT businessid, businessname, industryid, location_id FROM businesses');
+        
+        $x = 0;
+        
+        while ($busResult = $bus->fetch()) 
+        {
+            try {
+                $naics = $conn->prepare('SELECT naics_id, naics_description FROM naics WHERE naics_id = ?');
+                $naics->execute([$busResult['industryid']]);
+                $naicsResult = $naics->fetch(PDO::FETCH_ASSOC);
+            } catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+            try {
+                $location = $conn->prepare('SELECT id, city, state_id FROM geodata WHERE id = ?');
+                $location->execute([$busResult['location_id']]);
+                $locResult = $location->fetch(PDO::FETCH_ASSOC);
+            } catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            echo "<h4><a href=\"index.php?loc=companyview&busid=".$busResult['businessid']."\" title=\"".$busResult['businessname']."\">".$busResult['businessname']."</a><br><small>";
+            echo "Location: ".$locResult['city'].", ".$locResult['state_id']." | Industry: ".$naicsResult['naics_description']."</small></h4>";
+            if ($x == 25) { break; }
+            $x++;
         }
-        try {
-            $location = $conn->prepare('SELECT id, city, state_id FROM geodata WHERE id = ?');
-            $location->execute([$busResult['location_id']]);
-            $locResult = $location->fetch(PDO::FETCH_ASSOC);
-        } catch(PDOException $e) {
-            echo $e->getMessage();
+    } else {
+        $bus = $conn->query('SELECT businessid, businessname, industryid, location_id FROM businesses');
+        
+        $x = 0;
+        
+        while ($busResult = $bus->fetch()) 
+        {
+            try {
+                $naics = $conn->prepare('SELECT naics_id, naics_description FROM naics WHERE naics_id = ?');
+                $naics->execute([$busResult['industryid']]);
+                $naicsResult = $naics->fetch(PDO::FETCH_ASSOC);
+            } catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+            try {
+                $location = $conn->prepare('SELECT id, city, state_id FROM geodata WHERE id = ?');
+                $location->execute([$busResult['location_id']]);
+                $locResult = $location->fetch(PDO::FETCH_ASSOC);
+            } catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            echo "<h4><a href=\"index.php?loc=companyview&busid=".$busResult['businessid']."\" title=\"".$busResult['businessname']."\">".$busResult['businessname']."</a><br><small>";
+            echo "Location: ".$locResult['city'].", ".$locResult['state_id']." | Industry: ".$naicsResult['naics_description']."</small></h4>";
+            if ($x == 25) { break; }
+            $x++;
         }
 
-        echo "<h4><a href=\"index.php?loc=companyview&busid=".$busResult['businessid']."\" title=\"".$busResult['businessname']."\">".$busResult['businessname']."</a><br><small>";
-        echo "Location: ".$locResult['city'].", ".$locResult['state_id']." | Industry: ".$naicsResult['naics_description']."</small></h4>";
-        if ($x == 25) { break; }
-        $x++;
     }
 } elseif (isset($_GET['busid']) && isset($_SESSION['username'])) {
     /*
@@ -59,6 +94,8 @@ if (!isset($_GET['busid']) && isset($_SESSION['username'])) {
         header('Location: index.php');
     }
     */
+} elseif (isset($_GET['bussearch'])) {
+
 } else {
     logevent('Tried to view a company without being logged in business id: '.$_GET['busid']);
     $conn=null;
