bd4b2707f4c85ec6b02265fc648ae7277bda425b
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2020-08-04 00:41:17 -0400

    .
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- scripts/fnddupes.php
+++ scripts/fnddupes.php
@@ -23,7 +23,7 @@ for ($x=0; $x < $totDupes-1; $x++) {
     }
     
     $y = 0;
-    while ($foundDupe = $findDupe->fetch()) {
+    while ($foundDupe = $findDupe->fetch(PDO::FETCH_ASSOC)) {
         if ($y == 0) {
             echo "First Record...Skipped.<br>";
         } else {
